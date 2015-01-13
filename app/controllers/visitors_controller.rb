912855e1facfc1a808f2910d4c804c752cd2596a
Ryan Kallis <ryan.kallis@gmail.com>
2015-01-13 11:18:59 +0200

    environment + visitor ::: update

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,11 @@
 class VisitorsController < ApplicationController
 
   def new
+    @DEBUG = "\033[1;34;40m[DEBUG]\033[0m " 
+    Rails.logger.debug @DEBUG + ': entering new method'
 
     @owner = Owner.new
-    render 'visitors/new'
+    Rails.logger.debug @DEBUG + ': Owner name is ' + @owner.name
   end
 
 end
