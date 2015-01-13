3cb956653866aa58e70835d56639be900dea719b
Ryan Kallis <ryan.kallis@gmail.com>
2015-01-13 12:49:19 +0200

    install SimpleForm with a Zurb Foundation option

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,22 @@
 class VisitorsController < ApplicationController
 
   def new
-    @DEBUG = "\033[1;34;40m[DEBUG]\033[0m " 
-    Rails.logger.debug @DEBUG + ': entering new method'
-
     @owner = Owner.new
-    Rails.logger.debug @DEBUG + ': Owner name is ' + @owner.name
-    render 'visitors/new'
   end
 
 end
+
+# vvvvvvvvvvvvvvv  DEMO Purposes    vvvvvvvvvvvvv
+# class VisitorsController < ApplicationController
+
+#   def new
+#     @DEBUG = "\033[1;34;40m[DEBUG]\033[0m " 
+#     Rails.logger.debug @DEBUG + ': entering new method'
+
+#     @owner = Owner.new
+#     Rails.logger.debug @DEBUG + ': Owner name is ' + @owner.name
+#     render 'visitors/new'
+#   end
+
+# end
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
